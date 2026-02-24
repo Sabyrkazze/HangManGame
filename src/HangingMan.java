2f699b1841b4a7645bdf1d0c511bc251996627f8
Sabyrkazze <blacksenpai777@gmail.com>
2026-02-24 14:02:44 +0500

    Implementing English version of the game

--- src/HangingMan.java
+++ src/HangingMan.java
@@ -3,7 +3,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class HangingMan {
-
     public String getGallowStateLine(int lineToRead){
         try {
             return Files.readAllLines(Paths.get("resources/gallow_state.txt")).get(lineToRead);
--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,20 @@ public class Main {
             try {
                 answer = startOrExit(scanner);
             } catch (InputMismatchException e) {
-                System.out.println("Тек 0 немесе 1 сандарын енгізіңіз!");
+                System.out.println("Type only numbers 1 or 0!");
                 scanner.nextLine();
                 continue;
             }
 
             if(answer == 0){
-                System.out.println("Сау болыңыз!");
+                System.out.println("Good Bye!");
                 break;
             }else if (answer != 1){
-                System.out.println("Тек 0 немесе 1 сандарын енгізіңіз!");
+                System.out.println("Type only numbers 1 or 0!");
                 continue;
             }
 
-            System.out.println("Ойынды бастайық! ");
+            System.out.println("Let's start! ");
             System.out.println();
 
             int difficulty;
@@ -33,19 +33,19 @@ public class Main {
                 try {
                     difficulty = chooseDifficulty(scanner);
                     if (difficulty != 1 && difficulty != 2 && difficulty != 3) {
-                        System.out.println("Тек 1, 2, 3 сандары берілуі керек!");
+                        System.out.println("Only numbers 1, 2, 3 must be typed!");
                         continue;
                     }
                     break;
                 } catch (InputMismatchException e) {
-                    System.out.println("1,2 немесе 3 сандарын ғана енгізіңіз.");
+                    System.out.println("Only numbers 1, 2, 3 must be typed!");
                     scanner.nextLine();
                 }
             }
 
             StringBuilder wrongLetters = new StringBuilder();
             String word = words.chooseAWord(getPathForDifficulty(difficulty)).toUpperCase();
-//            String word = "ЫНТЫМАҚТАСТЫҚ";
+//            String word = "LIGHTNING";
             ArrayList<Character> hiddenWord = fillWithUnderscore(word);
 
             int loseCount = 0;
@@ -61,7 +61,7 @@ public class Main {
                 showWrongLetters(wrongLetters);
                 getGallowState(lineToRead);
 
-                System.out.println("Әріпті енгізіңіз: ");
+                System.out.println("Type the letter: ");
                 String input = scanner.next().toUpperCase();
                 char guessingLetter = input.charAt(0);
 
@@ -89,7 +89,6 @@ public class Main {
                 getGallowState(lineToRead);
                 notifyDefeat(word);
             }
-
         }
     }
 
@@ -101,7 +100,7 @@ public class Main {
         } else if (levelOfDifficulty == 3) {
             return "resources/hard_words.txt";
         }else {
-            System.out.println("Тек 1, 2, 3 сандары берілуі керек!");
+            System.out.println("Only numbers 1, 2, 3 must be typed!");
             return null;
         }
     }
@@ -110,7 +109,7 @@ public class Main {
         if(!Character.isDigit(guessingLetter)){
             return true;
         }else{
-            System.out.println("Өтінемін, әріп енгізіңіз.");
+            System.out.println("Please, enter a letter.");
             System.out.println();
             return false;
         }
@@ -120,25 +119,25 @@ public class Main {
         if(input.length() == 1){
             return true;
         }else{
-            System.out.println("Сіз тым ұзын жол енгіздіңіз. Тек бір әріп енгізіңіз.");
+            System.out.println("Type only one letter!");
             System.out.println();
             return false;
         }
     }
     private static void showHiddenWord(ArrayList<Character> hiddenWord){
-        System.out.print("Жасырылған сөз: ");
+        System.out.print("Hidden word: ");
         System.out.println(hiddenWord);
     }
     private static void showWrongLetters(StringBuilder wrongLetters) {
-        System.out.println("Қате әріптер: " + wrongLetters);
+        System.out.println("Wrong letters: " + wrongLetters);
     }
 
-    private static int chooseDifficulty(Scanner scanner){
+    private static int chooseDifficulty(Scanner scanner) {
         int difficulty;
-        System.out.println("Ойын деңгейін таңдаңыз:");
-        System.out.println("1 - Оңай");
-        System.out.println("2 - Орташа");
-        System.out.println("3 - Қиын");
+        System.out.println("Choose difficulty: ");
+        System.out.println("1 - Easy");
+        System.out.println("2 - Medium");
+        System.out.println("3 - Hard");
         difficulty = scanner.nextInt();
         scanner.nextLine();
         return difficulty;
@@ -146,8 +145,8 @@ public class Main {
 
     private static int startOrExit(Scanner scanner) {
         int answer;
-        System.out.println("Ойынды бастау - 1");
-        System.out.println("Шығу - 0");
+        System.out.println("Start the game - 1");
+        System.out.println("Exit - 0");
         answer = scanner.nextInt();
         scanner.nextLine();
         return answer;
@@ -162,9 +161,9 @@ public class Main {
     }
 
     private static void congratulateUser(String word) {
-        System.out.println("  ЖЕҢІС !");
+        System.out.println("  VICTORY !");
         System.out.println();
-        System.out.println("Жасырылған сөз: " + word.toUpperCase());
+        System.out.println("Hidden word: " + word.toUpperCase());
         System.out.println();
     }
 
@@ -178,10 +177,10 @@ public class Main {
     }
 
     private static void notifyDefeat(String word) {
-        System.out.println("  Таба алмадыңыз:( \n" +
-                "  Ойын аяқталды.");
+        System.out.println("  You couldn't find the letter:( \n" +
+                "  GAME OVER.");
         System.out.println();
-        System.out.println("Жасырылған сөз: " + word.toUpperCase());
+        System.out.println("Hidden word: " + word.toUpperCase());
         System.out.println();
     }
 
